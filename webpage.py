15a11d72b37d1612743218573b200a69f4dcbb89
Gabriel Muccillo Hartz <gabrielhartz@gmail.com>
2023-07-06 18:50:39 +0000

    added webhook functionality

--- webpage.py
+++ webpage.py
@@ -1,6 +1,16 @@
-from flask import Flask, render_template, url_for
+import git
+from flask import Flask, render_template, url_for, flash, redirect, request
 
 app = Flask(__name__)
+@app.route("/update_server", methods=['POST'])
+def webhook():
+    if request.method == 'POST':
+        repo = git.Repo('/home/skyshade/FlaskTesting')
+        origin = repo.remotes.origin
+        origin.pull()
+        return 'Updated PythonAnywhere successfully', 200
+    else:
+        return 'Wrong event type', 400
 
 @app.route("/")
 @app.route("/home")
